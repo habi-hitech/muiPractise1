826d3906d6086baa74222459d3e45004e9f1fb80
unknown <habi@hitechvalley.com.au>
2024-01-04 08:22:49 +0545

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import { Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 const App = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
     <>
       <Typography variant="h4" align="center" fontWeight="bold" noWrap>
@@ -15,7 +30,35 @@ const App = () => {
       >
         Secondary
       </Button>
-      <TextField />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="name"
+          placeholder="Name"
+          variant="outlined"
+          sx={{ pr: 3 }}
+          value={inputs.name}
+          onChange={handleChange}
+        />
+        <TextField
+          name="email"
+          placeholder="Email"
+          variant="standard"
+          sx={{ pr: 3 }}
+          onChange={handleChange}
+        />
+        <TextField
+          password="password"
+          placeholder="Password"
+          variant="filled"
+          type="password"
+          sx={{ pr: 3 }}
+          onChange={handleChange}
+        />
+        <Typography>
+          {inputs.name} + {inputs.email}
+        </Typography>
+        <Button type="submit">Sumbit</Button>
+      </form>
     </>
   );
 };
